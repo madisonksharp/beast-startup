194453563193bcbc8564d5f1555982d6933c4f47
Madison Sharp <madisonksharp@gmail.com>
2024-04-15 20:18:26 -0600

    starting mongo DB

--- mock.js
+++ mock.js
@@ -6,43 +6,43 @@ const Users = [
     name: "Abram",
     email: "abethebabe@gmail.com",
     profilePic: "/images/swimming.jpg",
-    goals: [
-      {
-        name: "Take Vitamins",
-        frequency: "daily",
-        streak: "2 days",
-      },
-      {
-        name: "Walk Dogs",
-        frequency: "daily",
-        streak: "3 days",
-      },
-      {
-        name: "Go to Gym",
-        frequency: "daily",
-        streak: "5 days",
-      },
-    ],
-    buddies: ["frogoildgear"],
+    // goals: [
+    //   {
+    //     name: "Take Vitamins",
+    //     frequency: "daily",
+    //     streak: "2 days",
+    //   },
+    //   {
+    //     name: "Walk Dogs",
+    //     frequency: "daily",
+    //     streak: "3 days",
+    //   },
+    //   {
+    //     name: "Go to Gym",
+    //     frequency: "daily",
+    //     streak: "5 days",
+    //   },
+    // ],
+    // buddies: ["frogoildgear"],
   },
   {
     username: "frogoildgear",
     name: "Eric",
     email: "froggy@gmail.com",
     profilePic: "/images/profile-f.png",
-    goals: [
-      {
-        name: "take vitamins",
-        frequency: "daily",
-        streak: "2 days",
-      },
-      {
-        name: "walk dogs",
-        frequency: "daily",
-        streak: "3 days",
-      },
-    ],
-    buddies: ["abethebabe"],
+    //   goals: [
+    //     {
+    //       name: "take vitamins",
+    //       frequency: "daily",
+    //       streak: "2 days",
+    //     },
+    //     {
+    //       name: "walk dogs",
+    //       frequency: "daily",
+    //       streak: "3 days",
+    //     },
+    //   ],
+    //   buddies: ["abethebabe"],
   },
 ];
 
